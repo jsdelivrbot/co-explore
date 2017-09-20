49e8d199eebffc0872c0d2b36d47882e3e1928ac
Alan Boulton <alan.boulton@gmail.com>
2017-09-20 01:04:59 +0100

    cleanup of unwanted stuff

--- install/co-explore-1.0/docker/hellodocker/Readme.md
+++
@@ -1,3 +0,0 @@
-# Simple docker image app
-Taken from (this site)[https://docs.docker.com/get-started/part2/#define-a-container-with-a-dockerfile].
-
--- install/co-explore-1.0/python/turtle/uses.py
+++
@@ -1,6 +0,0 @@
-from mylib import printhello
-printhello(", my love!" + " This" + " is so" + " cool!\n" + "Good night!")
-
-
-from mylib import drawCircles
-drawCircles("red", 5, 100)
